a968efd84305fddfbfb0957546adc94ef4f73694
sksayril <sksayril123@gmail.com>
2025-10-28 12:44:16 +0530

    fixed the issue

--- scripts/checkDatabases.js
+++ scripts/checkDatabases.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function checkDatabases() {
   try {
-    const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/skystar-admin'
+    const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://cripcocode:sksayril123@45.129.86.243:27017/skystarconact'
     console.log('Connecting to:', MONGODB_URI)
     
     await mongoose.connect(MONGODB_URI)
